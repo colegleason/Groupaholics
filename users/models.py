eec7020f722c739df0b4de9849c56f159e098834
zach297 <zach297@gmail.com>
2011-09-02 18:56:07 -0500

    Add profiles and tags.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+#Profile.objects.all().filter(profiletag__title='python')[0].user.username
+class Profile(models.Model):
+	user = models.OneToOneField(User)
+	bio = models.TextField()
+	
+class Tag(models.Model):
+	title = models.CharField(max_length=20)
+	level = models.PositiveIntegerField()	
+	
+class ProfileTag(Tag):
+	profile = models.ForeignKey('Profile')
+	
