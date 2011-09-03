cf73703d94320fcee73b5be4512d6b9b9111053b
Cole Gleason <colewashere@gmail.com>
2011-09-02 19:55:51 -0500

    working on shit

--- settings.py
+++ settings.py
@@ -116,7 +116,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'projects',
     # Uncomment the next line to enable the admin:
      'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
--- views.py
+++ views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 
 def main(request):
     context = {}
     return render_to_response('main.html', context)
+
+def register(request):
+    context = {}
+    return render_to_response('register.html', context)
+
+def profile(request, username):
+    user = get_object_or_404(User, username=username)
+    context = {'user': user}
+    return render_to_response('profile.html', context)
+
+def profile_edit(request):
+    context = {}
+    return render_to_response('profile_edit.html', context)
+
+def project(request):
+    #TODO news post code to get latest posts
+    context = {}
+    return render_to_response('project.html', context)
+
+def project_edit(request):
+    context = {}
+    return render_to_response('project_edit.html', context)
