da2588e79a86e89a81a3114bd7eb39024649b701
Cole Gleason <colewashere@gmail.com>
2011-09-02 21:05:23 -0500

    added a bunch to urls.py

--- urls.py
+++ urls.py
@@ -7,6 +7,12 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', views.main),
+    url(r'^register$', views.register),
+    url(r'^profile/(?P<username>\w+', views.profile),
+    url(r'^account/edit$', views.profile_edit),
+    url(r'^project/(?P<pk_id>\d+$', views.project),
+    url(r'^project/(?P<pk_id>\d+/edit$', views.project_edit),
+    url(r'^search$', views.search),
     # url(r'^cs_comp/', include('cs_comp.foo.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
      url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- views.py
+++ views.py
@@ -14,14 +14,21 @@ def profile(request, username):
     return render_to_response('profile.html', context)
 
 def profile_edit(request):
+    # authentication needed
     context = {}
     return render_to_response('profile_edit.html', context)
 
-def project(request):
+def project(request, pk_id):
     #TODO news post code to get latest posts
     context = {}
     return render_to_response('project.html', context)
 
-def project_edit(request):
+def project_edit(request, pk_id):
+    # authentication needed
     context = {}
     return render_to_response('project_edit.html', context)
+
+def search(request):
+    query = request.GET.get('q')
+    context = {'query' : query}
+    return render_to_response('search.html', context)
