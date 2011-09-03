34d6a56ebcaca34d787ea8c6341d0299cfbc18c9
zach297 <zach297@gmail.com>
2011-09-02 19:59:00 -0500

    Finished models (alpha).

--- users/models.py
+++ users/models.py
@@ -7,11 +7,31 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
 	user = models.OneToOneField(User)
 	bio = models.TextField()
+	skill_tags = models.ManyToManyField('SkillTag')
 	
+CATEGORY_CHOICES = (
+	(0, 'Project'),
+	(1, 'Organization')
+)
+class Project(models.Model):
+	description = models.TextField()
+	tags = models.ManyToManyField('Tag')
+	skill_tags = models.ManyToManyField('SkillTag')
+	members = models.ManyToManyField('Profile', related_name='member_of')
+	admins = models.ManyToManyField('Profile', related_name='admin_of')
+	category = models.IntegerField(choices=CATEGORY_CHOICES)
+	parent_project = models.ForeignKey('Project', null=True, blank=True)
+	
+class NewsEntry(models.Model):
+	pub_date = models.DateTimeField(auto_now_add=True)
+	author = models.ForeignKey('Profile')
+	title = models.CharField(max_length=100)
+	body = models.TextField()
+
 class Tag(models.Model):
 	title = models.CharField(max_length=20)
-	level = models.PositiveIntegerField()	
 	
-class ProfileTag(Tag):
-	profile = models.ForeignKey('Profile')
+class SkillTag(models.Model):
+	title = models.CharField(max_length=20)
+	level = models.PositiveIntegerField()	
 	
